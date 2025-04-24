5c14d05b4c3d546b2e992de8cce9b4a988434db4
laya-sree <kolathur.layasree@kalvium.community>
2025-04-24 09:49:28 +0530

    completed

--- index.js
+++ index.js
@@ -1,15 +1,30 @@
 const express = require('express');
-const { resolve } = require('path');
+const app= express();
+const PORT=8000;
 
-const app = express();
-const port = 3010;
+app.use(express.json())
 
-app.use(express.static('static'));
+app.post('/signup',(req,res) =>{
+    const {username ,email,password,dob} = req.body
+    if(!username){
+        return res.status(400).json({message:"username not found"})
+    }
+    if(!email){
+        return res.status(400).json({message:"email not found"})
+    }
+    if(!dob){
+        return res.status(400).json({message:"dob not found"})
+    }
+    if(!password || password.length < 8 || password.length > 16){
+        return res.status(400).json({message:"password not valid and it should be greater than 8 character or less than 16 character"})
+    }
+    res.status(200).json({message:"signup successful"})
+})
 
-app.get('/', (req, res) => {
-  res.sendFile(resolve(__dirname, 'pages/index.html'));
-});
+app.get('/',(req,res)=>{
+    res.send("welcome to signup page")
+})
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+app.listen(PORT,()=>{
+    console.log(`server is running on http://localhost:${PORT}`)
+})
